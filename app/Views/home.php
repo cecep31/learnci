69b65f3a599d98a070b576e4411681028de1d754
cecep31 <cecepjanuardi31@gmail.com>
2021-08-12 11:26:49 +0700

    layout learning

--- app/Views/home.php
+++ app/Views/home.php
@@ -1,5 +1,7 @@
 <?= $this->extend('layouts/base') ?>
 <?= $this->section('content') ?>
-    welcome <br>
+<div>
+    <h2>welcpme home blog</h2>
     <button class="ui secondary button">button</button>
+</div>
 <?= $this->endSection() ?>
