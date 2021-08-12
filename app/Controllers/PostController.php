1231e30ff9d51a06f92f199cdd4dae0e8b8f1951
cecep31 <cecepjanuardi31@gmail.com>
2021-08-12 10:56:35 +0700

    first

---
+++ app/Controllers/PostController.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class PostController extends BaseController{
+    public function index(){
+        return view('home');
+    }
+    public function addpost(){
+        return view('home');
+    }
+    public function updatepost(){
+        return view('home');
+    }
+    public function dellatepost(){
+        return view('home');
+    }
+}
+?>
---
+++ app/Views/home.php
@@ -0,0 +1,5 @@
+<?= $this->extend('layouts/base') ?>
+<?= $this->section('content') ?>
+    welcome <br>
+    <button class="ui secondary button">button</button>
+<?= $this->endSection() ?>
