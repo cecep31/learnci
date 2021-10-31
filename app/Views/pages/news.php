582dbe5997c9afa0302f7f2eb7f40a294751bdd9
cecep januardi <cecepjanuardi31@gmail.com>
2021-10-31 20:51:28 +0700

    a

--- app/Views/pages/news.php
+++ app/Views/pages/news.php
@@ -1,7 +1,7 @@
 <?= $this->extend('/layouts/base') ?>
 <?= $this->section('content') ?>
 <div class="mx-auto px-5 pt-2">
-    <div class="btn btn-info my-4 justify-content-end"><a class="text-decoration-none" href="<?= base_url('news-add') ?>">Tambah Data</a> </div>
+    <div class=" my-4 d-flex justify-content-end"><a class="btn btn-info text-decoration-none" href="<?= base_url('news-add') ?>">Tambah Data</a> </div>
     <?php foreach ($newes as $news) : ?>
         <div class="card">
             <div class="card-header">
